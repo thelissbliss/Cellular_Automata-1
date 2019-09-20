a9ae92930502c80bc811e54916835aeeea0785d9
laurasolorio98 <31724167+laurasolorio98@users.noreply.github.com>
2019-09-19 23:04:43 -0700

    Final draw_stuff.js

--- assets/draw-stuff.js
+++ assets/draw-stuff.js
@@ -1,11 +1,16 @@
-// import java.util.Random;
+/* Laura Solorio laurasolorio98@csu.fullerton.edu
+   Alyssa Bright alyssabright@csu.fullerton.edu
 
-// Draw stuff
-// ------------------------------------------------------------
-let ruleset = [0, 0, 0 , 0, 1, 1, 0, 0];
-let stateArray = [ ];
+   File Description: This javascript file contains all functions
+	1. Draw Grid
+	2. Determines the direction for North, East, South, or West
+       Displays white triangle
+	3. Colors in the cell
+	4. Changes current state of the cell; Change color; change ant
+	   direction
+*/
 
-// =====================================================  draw_grid ====
+// ======  draw_grid ====
 function draw_grid( rctx, rminor, rmajor, rstroke, rfill  ) 
 {
     rctx.save( );
@@ -34,71 +39,11 @@ function draw_grid( rctx, rminor, rmajor, rstroke, rfill  )
     rctx.restore( ); 
 }
 
-// Determine next move
-private static int nextMove(int min, int max) {
-
-	if (min >= max) {
-		throw new IllegalArgumentException("max > min");
-	}
-
-	Random r = new Random();
-	return r.nextInt((max - min) + 1) + min;
-}
-
-// Next move instructions
-function nextInstructions(int r) {
-	if (r==0){
-		// black = turn right
-		if (direction = 0) { // North turn East
-			direction = 1;
-
-		} else if (direction = 1) { // East turn South
-			direction = 2;
-
-		} else if (direction = 2) { // South turn West
-			direction = 3;
-		} else { // West turn North
-			direction = 0;
-		}
-		
-	} else if (r==1) {
-		// red = turn right
-		if (direction = 0) { // North turn East
-			direction = 1;
-
-		} else if (direction = 1) { // East turn South
-			direction = 2;
-
-		} else if (direction = 2) { // South turn West
-			direction = 3;
-		} else { // West turn North
-			direction = 0;
-		}
-
-	} else {
-		// yellow or blue = turn left
-		if (direction = 0) { // North turn West
-			direction = 3;
-
-		} else if (direction = 1) { // East turn North
-			direction = 0;
-
-		} else if (direction = 2) { // South turn East
-			direction = 1;
-
-		} else { // West turn South
-			direction = 2;
-		}
-
-	}
-}
-
 //Determines the direction for North, East, South, or West
 //Displays white triangle
 function pointThis(direction, triangle, yaxis,xaxis) {
 	//Points North on a given input
     if(direction==0){
-		//direction = 0;
 		triangle.save();
 		triangle.beginPath();
   		triangle.moveTo(xaxis-6, yaxis+6);
@@ -112,10 +57,9 @@ function pointThis(direction, triangle, yaxis,xaxis) {
 		triangle.restore();
 		
 	}
-	//Points east on a given input
+	
+	//Points East on a given input
 	else if(direction==1){
-		//east
-		//direction = 1;
 		triangle.save();
 		triangle.beginPath();
   		triangle.moveTo(xaxis-6, yaxis-6);
@@ -129,6 +73,7 @@ function pointThis(direction, triangle, yaxis,xaxis) {
 		triangle.restore();
 		
 	}
+	
 	//Point South on a given direction
 	else if(direction==2){
 		triangle.save();
@@ -144,6 +89,7 @@ function pointThis(direction, triangle, yaxis,xaxis) {
 		triangle.restore();
 		
 	}
+	
 	//Points West on a given input
 	else if(direction==3){ 
 		triangle.save();
@@ -158,19 +104,179 @@ function pointThis(direction, triangle, yaxis,xaxis) {
 		triangle.stroke();
 		triangle.restore();
 	}
+	
+	//Check if ant values are not in above peramiters
+	//Message will be displayed
+	else{
+		throw "Too big";
+	}
 }
-//Do for further config.
-//Still need to choose what colors will be used
 
-function fillSquare(square, yaxis,xaxis){
+//Colors in the cell
+function fillSquare(square, yaxis,xaxis, color, colorCount){
 	square.beginPath();
 	square.rect(xaxis, yaxis, 10, 10);
-	/**This may turn into a conditional statment
-	  Depending on the color*****/
-	square.fillStyle = 'red';
+	square.fillStyle = color[colorCount];
 	square.fill();
 }
 
-function walkto(context, yaxis,xaxis){
+//Changes current state of the cell
+function changeStateArray(square, xaxis, yaxis, color,direction,stateArray) {
+	var counter =0;
+	var i = xaxis/10;
+	var j= yaxis/10;
 	
+	//loops though until it hits1000
+	while(counter<=1000){
+		//Ant moved to a cell with a value of 0 or null.
+		//Cell value will now change to 1
+		//The color and position of the triangle will now change
+		//The new cell color will change.
+		if (stateArray[i][j] == 0 || stateArray[i][j] ==null){
+			stateArray[i][j]=1;
+			//calls cell coloring function
+			fillSquare(square, yaxis,xaxis, color, 1)
+			//triange will point to direction of next cell
+			//pointThis(direction, context, yaxis,xaxis);
+			//move to next cell
+			if(i<40 && j <40){
+				if(direction==0){
+					xaxis=xaxis+10;
+					i++;
+					direction=1;
+				}
+				else if(direction==1){
+					j++;
+					yaxis=yaxis+10
+					direction=2;
+				}
+				else if(direction==2){
+					i--;
+					xaxis=xaxis-10;
+					direction=3;
+				}
+				else if(direction==3){
+					j--;
+					yaxis=yaxis-10;
+					direction=0;
+				}
+				else{
+					throw "Too big";
+				}
+			}
+		}
+		
+		//Ant moved to a cell with a value of 1.
+		//Cell value will now change to 2
+		//The color and position of the triangle will now change
+		//The new cell color will change.
+		else if(stateArray[i][j] == 1){
+			stateArray[i][j]=2;
+			fillSquare(square, yaxis,xaxis, color, 2)
+			//pointThis(direction, context, yaxis,xaxis);
+			//move to next cell
+			if(i<40 && j <40){
+				if(direction==0){
+					xaxis=xaxis-10;
+					i--;
+					direction=3;
+				}
+				
+				else if(direction==1){
+					j--;
+					yaxis=yaxis-10
+					direction=0;
+				}
+				else if(direction==2){
+					i++;
+					xaxis=xaxis+10;
+					direction=1;
+				}
+				else if(direction==3){
+					j++;
+					yaxis=yaxis+10;
+					direction=2;
+				}
+				else{
+					throw "Too big";
+				}
+			}
+	}
+		
+		//Ant moved to a cell with a value of 2.
+		//Cell value will not change to 3
+		//The color and position of the triangle will now change
+		//The new cell color will change.
+		else if(stateArray[i][j] == 2){
+				stateArray[i][j]=3;
+				fillSquare(square, yaxis,xaxis, color, 3)
+				//pointThis(direction, context, yaxis,xaxis);
+				//move to next cell
+				if(i<40 && j <40){
+					if(direction==0){
+						xaxis=xaxis-10;
+						i--;
+						direction=3;
+					}
+
+					else if(direction==1){
+						j--;
+						yaxis=yaxis-10
+						direction=0;
+					}
+					else if(direction==2){
+						i++;
+						xaxis=xaxis+10;
+						direction=1;
+					}
+					else if(direction==3){
+						j++;
+						yaxis=yaxis+10;
+						direction=2;
+					}
+					else{
+						throw "Too big";
+					}
+				}
+		}
+		
+		//Ant moved to a cell with a value greater than 3.
+		//Cell value will now change to 0
+		//The color and position of the triangle will now change
+		//The new cell color will change.
+		else{
+			stateArray[i][j]=0;
+			fillSquare(square, yaxis,xaxis, color, 0)
+			//pointThis(direction, context, yaxis,xaxis);
+			//move to next cell
+			if(i<40 && j <40){
+				if(direction==0){
+					xaxis=xaxis+10;
+					i++;
+					direction=1;
+				}
+
+				else if(direction==1){
+					j++;
+					yaxis=yaxis+10
+					direction=2;
+				}
+				else if(direction==2){
+					i--;
+					xaxis=xaxis-10;
+					direction=3;
+				}
+				else if(direction==3){
+					j--;
+					yaxis=yaxis-10;
+					direction=0;
+				}
+				else{
+					throw "Too big";
+				}
+			}
+		}
+	//loop counter
+	counter++;
+	}
 }
